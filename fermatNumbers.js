14c9aea6f123f2f98d285a5798bd16b5abb9e073
Isaac Carter <icarter1391@gmail.com>
2018-11-05 16:23:01 -0500

    Update fermatNumbers.js
    Removed var and replaced with let.

--- fermatNumbers.js
+++ fermatNumbers.js
@@ -1,13 +1,13 @@
 (function() {
-	var limit = 0;	
+	let limit = 0;	 
 
 	while ( limit < 10) {
-		let firstPower = Math.pow(2, limit);
-		let secondPower = Math.pow(2, firstPower);
-		let fermatNumber = secondPower + 1;
+		let firstPower = Math.pow(2, limit),
+		    secondPower = Math.pow(2, firstPower),
+		    fermatNumber = secondPower + 1;
 
 		console.log("Fermat number for F(" + limit + ") is " + fermatNumber);
-
+		
 		limit++; 
 	}
 })();
